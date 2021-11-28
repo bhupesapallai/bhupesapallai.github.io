4b21f1efb16a4b074c89748e8ecf4fc03522d936
Anmol Naik <en19cs304007@medicaps.ac.in>
2021-11-28 23:00:24 +0530

    Testimonial

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 var silder = $(".owl-carousel");
 silder.owlCarousel({
-autoplay: true,
+autoplay: false,
 autoplayTimeout: 3500,
 autoplayHoverPause: false,
 items: 1,
@@ -13,11 +13,11 @@ dots: true,
 loop: true,
 responsive: {
 0: { items: 1 },
-480: { items: 2 },
-575: { items: 2 },
-768: { items: 2 },
-991: { items: 2 },
-1200: { items: 3 }
+480: { items: 1 },
+575: { items: 1 },
+768: { items: 1 },
+991: { items: 1 },
+1200: { items: 1 }
 }
 });
 });
