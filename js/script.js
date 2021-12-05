d55250e5baa4eed069e826dbd7a890b6e010bb30
Anmol Naik <en19cs304007@medicaps.ac.in>
2021-12-05 20:04:50 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,7 +27,6 @@ var loader;
 function myFunction() {
   loader = setTimeout(showPage, 500);
 }
-
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("body").style.display = "flex";
